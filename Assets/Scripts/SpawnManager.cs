687aa487b2fe4b8820ea57d821778455b9cfd7ba
nhatvm10 <nhatvm10@fpt.com.vn>
2023-03-03 14:01:13 +0700

    sửa spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -27,24 +27,8 @@ public class SpawnManager : MonoBehaviour
     }
     void Update()
     {
-        endPoint = Gennerate();
         
     }
 
-    public Vector3 Gennerate()
-    {
-        float screenWidth = Screen.width;
-        float screenHeight = Screen.height;
-        // save screen edges in world coordinates
-        float screenZ = -Camera.main.transform.position.z;
-        Vector3 lowerLeftCornerScreen = new Vector3(0, 0, screenZ);
-        Vector3 upperRightCornerScreen = new Vector3(screenWidth, screenHeight, screenZ);
-        Vector3 lowerLeftCornerWorld = Camera.main.ScreenToWorldPoint(lowerLeftCornerScreen);
-        Vector3 upperRightCornerWorld = Camera.main.ScreenToWorldPoint(upperRightCornerScreen);
-        float screenLeft = lowerLeftCornerWorld.x;
-        float screenRight = upperRightCornerWorld.x;
-        float screenTop = upperRightCornerWorld.y;
-        float screenBottom = lowerLeftCornerWorld.y;
-        return new Vector3(Random.Range(screenLeft, screenRight), Random.Range(screenBottom, screenTop), -Camera.main.transform.position.z);
-    }
+    
 }
