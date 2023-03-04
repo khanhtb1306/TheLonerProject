4accb0cf6d3e5652e278b9c6629c3f353f24ab17
nhatvm10 <nhatvuminh00@gmail.com>
2023-03-05 01:41:28 +0700

    sửa prefabs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -14,9 +14,6 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     public void BuffSpawn(Transform tf)
     {
-       
-       
-       
         int r = Random.Range(0, 10);
         if(r <= 2)
         {
