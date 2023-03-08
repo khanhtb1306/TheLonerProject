2328c93a42d5f8e8a8a4051da065a41e458699b2
Sull1298 <tranan2998@gmail.com>
2023-03-09 00:15:28 +0700

    remove void Ontrigger

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -191,29 +191,6 @@ public class Player : MonoBehaviour
             GameManager.instance.EndGame();
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Weapon weapon = collision.GetComponent<Weapon>();
-        Buff buff = collision.GetComponent<Buff>();
-        Bullet bullet = collision.GetComponent<Bullet>();
-        if (weapon != null)
-        {                                                                                                                                                                                                                         
-            ChangeWeapon(weapon);
-            Destroy(collision.gameObject);
-        }
-        if (buff != null)                                                                                                                               
-        {
-            ChangeBuffSkill(buff);
-            GameManager.instance.skillButton.ChangeAvatar();
-            Destroy(collision.gameObject);
-        }
-        if(bullet != null)
-        {
-            TakeDamge(bullet.damage);
-            Destroy(collision.gameObject);
-        }
-    }
     
     private IEnumerator Undead(float timeDuration, Player player)
     {
