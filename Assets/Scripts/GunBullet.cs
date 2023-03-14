2cf4cb229466385ba1e822152e279d28bc61976f
khanhtb1306 <trinhbaokhanh1306@gmail.com>
2023-03-14 10:57:44 +0700

    update Prefabs bullet

--- Assets/Scripts/GunBullet.cs
+++ Assets/Scripts/GunBullet.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BulletStyle
+{
+    BulletPistol,
+    BulletFast,
+    BulletStrong,
+    Bom,
+}
+
 public class GunBullet : MonoBehaviour
 {
     public float bulletLifeTime = 3f;
     public float damage = 10;
     // Update is called once per frame
+
+    public float bulletForce = 10;
+
+    public void Fire()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * bulletForce, ForceMode2D.Impulse);
+    }
+
     void Update()
     {
         DestroyBullet();
